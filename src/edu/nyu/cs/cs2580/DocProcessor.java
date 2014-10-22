dd126556aeabcea398f4a6961ff27efa5cc3b745
Angie Chiang <angieyayabird@gmail.com>
2014-10-22 05:25:09 -0400

    refine

--- src/edu/nyu/cs/cs2580/DocProcessor.java
+++ src/edu/nyu/cs/cs2580/DocProcessor.java
@@ -2,15 +2,10 @@ package edu.nyu.cs.cs2580;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
 import java.util.Scanner;
-import java.util.regex.Pattern;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
 import java.io.FileReader;
 import java.io.File;
 import java.io.IOException;
